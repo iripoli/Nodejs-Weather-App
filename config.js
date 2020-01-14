f8f746294031c3b0fa500c4fbbdadd3ad5d11a8a
iripoli <iripoli96@gmail.com>
2020-01-14 16:39:31 +0100

    Weather app con node y express version 1-0-0 falta aun cambiar los iconos

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants= {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "40a84a66c31344647aa118bb6232a3e5"
+    }
+}
+
+module.exports=constants;
